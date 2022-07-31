3889c6d16f85c04dce54ad7752a278e42e8a3470
zhouyuyann <zhouyuyan1021@gmail.com>
2022-07-31 11:06:46 -0400

    test hello

--- src/main/java/com/yuyanzhou/easy_book/EasyBookApplication.java
+++ src/main/java/com/yuyanzhou/easy_book/EasyBookApplication.java
@@ -2,12 +2,19 @@ package com.yuyanzhou.easy_book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class EasyBookApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EasyBookApplication.class, args);
     }
 
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello sprint boot";
+    }
 }
